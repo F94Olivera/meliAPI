e2bcde7ebb1bbc8cd29686746888dd73df630e42
Facundo Olivera <f94olivera@gmail.com>
2023-05-11 16:50:34 -0300

    new ref from Show to Venue

--- models/Show.js
+++ models/Show.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-
+const Venue = require('./Venue');
 const showSchema = new mongoose.Schema({
   name: String,
   venue: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Venue'
+    ref: Venue
   },
   performances: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Performance'
   }]
 });
-showSchema.index({ _id: 1 });
 
 module.exports = mongoose.model('Show', showSchema);
+mongoose.set({ debug: true });
