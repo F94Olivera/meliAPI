8addf5d018e154a9ef8b4e504a642f78827877d3
Facundo Olivera <f94olivera@gmail.com>
2023-05-11 16:56:06 -0300

    Fix reservationService

--- services/reservationService.js
+++ services/reservationService.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
 const Reservation = require('../models/Reservation');
 const Show = require('../models/Show');
+const Performance = require('../models/Performance');
+const mongoose = require('mongoose');
 
 exports.createReservation = async (reservationData) => {
   const { showId, performanceId, customerDni, customerName, seats } = reservationData;
@@ -9,38 +10,51 @@ exports.createReservation = async (reservationData) => {
   session.startTransaction();
 
   try {
-    const show = await Show.findById(showId).session(session);
+    const show = await Show.findById(showId).populate('venue').session(session);
     if (!show) {
       throw new Error('Show no encontrado');
     }
 
-    const performance = show.performances.id(performanceId);
+    const performance = await Performance.findById(performanceId).session(session);
     if (!performance) {
       throw new Error('Performance no encontrada');
+    }
+
+    const reservationSeats = [];
+
+    for (const seat of seats) {
+      const section = show.venue.sections.find(s => s._id.toString() === seat.sectionId);
+      const seatToUpdate = section.seats.find(s => s._id.toString() === seat.seatId);
+
+      if (!section || !seatToUpdate) {
+        throw new Error('Sección o asiento no encontrados');
+      }
+
+      const availableSeat = performance.availableSeats.find(s => s.seat.toString() === seatToUpdate._id.toString());
+      if (!availableSeat || availableSeat.reserved) {
+        throw new Error('El asiento ya está reservado');
+      }
+
+      reservationSeats.push({
+        section: section.name,
+        row: seatToUpdate.row,
+        number: seatToUpdate.number,
+        price: seatToUpdate.price
+      });
+
+      availableSeat.reserved = true;
+      await performance.save({ session });
     }
 
     const reservation = new Reservation({
       show: showId,
       performance: performanceId,
-      customerDni,
-      customerName,
-      seats
+      dni: customerDni,
+      name: customerName,
+      seats: reservationSeats
     });
 
     const savedReservation = await reservation.save({ session });
-
-    for (const seat of seats) {
-      const section = show.venue.sections.id(seat.section_id);
-      const seatToUpdate = section.seats.id(seat.seat_id);
-
-      if (seatToUpdate.reserved) {
-        throw new Error('El asiento ya está reservado');
-      }
-
-      seatToUpdate.reserved = true;
-    }
-
-    await show.save({ session });
 
     await session.commitTransaction();
     session.endSession();
